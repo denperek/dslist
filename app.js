af1fb9ba979979eff2e3b21c689b578365f51b6d
Den Perek <denperek@gmail.com>
2021-07-22 15:51:52 +0300

    make it better

---
+++ app.js
@@ -0,0 +1,85 @@
+const Express = require('express');
+const BodyParser = require('body-parser');
+const Mongoose = require('mongoose');
+const cors = require('cors');
+
+
+
+var app = Express();
+
+app.use(cors(
+    {
+    "origin": "*",
+    "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
+    "preflightContinue": false,
+    "optionsSuccessStatus": 204
+  }
+  ));
+
+//Mongoose.connect('mongodb://localhost:27017/dslist?retryWrites=true&w=majority', { useNewUrlParser: true }).then(() => console.log('Connected to DB...')).catch(err => console.error(err));
+Mongoose.connect('mongodb+srv://denperek:yakvut7@cluster0-as9x9.mongodb.net/dslist?retryWrites=true&w=majority', { useNewUrlParser: true }).then(() => console.log('Connected to DB...')).catch(err => console.error(err));
+
+
+app.use(BodyParser.json());
+app.use(BodyParser.urlencoded({extended: true}));
+
+const PersonModel = Mongoose.model("person", {
+    department: String,
+    fio: String,
+    storageType: String,
+    expirationDate: String
+	});
+
+app.post("/person", async (request, response, next) => {
+	try {
+        var person = new PersonModel(request.body);
+        var result = await person.save();
+        response.send(result);
+        
+	} catch (error) {
+    	response.status(500).send(error); 
+	}
+});
+
+app.get("/people", async (request, response, next) => {
+    try {
+        var result = await PersonModel.find().exec();
+        response.send(result);
+    } catch (error) {
+    	response.status(500).send(error); 
+	}
+});
+
+app.get("/person/:id", async (request, response, next) => {
+    try {
+        var person = await PersonModel.findById(request.params.id).exec();
+        response.send(person);
+    } catch (error) {
+    	response.status(500).send(error); 
+	}
+});
+
+app.put("/person/:id", async (request, response, next) => {
+    try {
+        var person = await PersonModel.findById(request.params.id).exec();
+        person.set(request.body);
+        var result = await person.save();
+        response.send(result);
+    } catch (error) {
+        response.status(500).send(error);
+    }
+});
+
+app.delete("/person/:id", async (request, response, next) => {
+    try {
+        var result = await PersonModel.deleteOne({ _id: request.params.id }).exec();
+        response.send(result);
+    } catch (error) {
+        response.status(500).send(error);
+    }
+});
+
+app.listen(3000, () => {
+	console.log('Listening at :3000...');
+});
+
--- script.js
+++ script.js
@@ -1,44 +1,124 @@
-
-
 let department = document.querySelector('#department');
 let fio = document.querySelector('#fio');
 let storageType = document.querySelector('#storage-type');
 let expirationDate = document.querySelector('#expiration-date');
 let submitButton = document.querySelector('#userinfo-submit');
 let infoContainer = document.querySelector('#info-container');
-let outputArea = document.querySelectorAll('.output-area');
+
+
+// ----------------------------ADD ROW FUNCTION-------------------------------------------------------
+function addRow(i, department, fio, storageType, expirationDate, userId) {
+
+  function createBlock(content, divClassName, i) {
+    let name = document.createElement('div');
+    name.textContent = `${content}`;
+    name.className = `output-area ${divClassName} ${divClassName}-${i}`;
+    return name;
+  }
+
+  function createDelBlock(divClassName, i) {
+    let imgDel = new Image(14, 14);
+    imgDel.src = 'del.png';
+    imgDel.className = `output-area ${divClassName} ${divClassName}-${i}`;
+    return imgDel;
+  }
+
+  let divDepartment = createBlock(department, 'divDepartment', i);
+  let divFio = createBlock(fio, 'divFio', i);
+  let divStorageType = createBlock(storageType, 'divStorageType', i);
+  let divExparationDate = createBlock(expirationDate, 'divExparationDate', i);
+  let divDelButton = createDelBlock('userinfo-delete', i);
+
+  divOutpuBlock = document.createElement('div');
+  divOutpuBlock.append(divDepartment, divFio, divStorageType, divExparationDate, divDelButton);
+  divOutpuBlock.className = `output-block output-block-${i}`;
+
+  infoContainer.prepend(divOutpuBlock);
+
+  let delButton = document.querySelector(`.userinfo-delete-${i}`);
+  delButton.addEventListener('click',
+
+    async function () {
+
+      // let sureDelete = confirm("Вы хотите удалить запись?");
+      // if (sureDelete) {
+
+      let response = await fetch(`http://localhost:3000/person/${userId}`, {
+        method: 'DELETE',
+        headers: {
+          'Content-Type': 'application/json;charset=utf-8'
+        },
+      }).then(() => {
+        let delOutputBlock = document.querySelector(`.output-block-${i}`);
+        infoContainer.removeChild(delOutputBlock);
+        console.log(`${fio} : Deleted!`);
+        //console.log(userId);
+      });
+      // }
+    }
+  );
+};
+
 
 let usersPool = [];
-
-class User{
-    constructor (dep, fam, storage, expDate) {
-        this.department = dep;
-        this.fio = fam;
-        this.storageType = storage;
-        this.expirationDate = expDate;
-    }
+class User {
+  constructor(dep, fam, storage, expDate) {
+    this.department = dep;
+    this.fio = fam;
+    this.storageType = storage;
+    this.expirationDate = expDate;
+  }
 }
 
-submitButton.addEventListener('click', function(){
-    let ggg = usersPool.length; 
-    console.log(ggg);
-   usersPool.push(new User(department.value, fio.value, storageType.value, expirationDate.value));
-   
-   
-//    let outputDiv = document.createElement('div');
-//    outputDiv.className = "output-area";
-//    outputDiv.textContent = 
 
-infoContainer.insertAdjacentHTML('afterbegin', `<div class="output-block"><div class="output-area">${usersPool[ggg].department}</div>
-<div class="output-area">${usersPool[ggg].fio}</div>
-<div class="output-area">${usersPool[ggg].storageType}</div>
-<div class="output-area">${usersPool[ggg].expirationDate}</div>
-</div>`);
-   
-   
-   // outputArea[0].textContent = usersPool[0].department;
-    // outputArea[1].textContent = usersPool[0].fio;
-    // outputArea[2].textContent = usersPool[0].storageType;
-    // outputArea[3].textContent = usersPool[0].expirationDate;
+// -----------------------SUBMIT-----------------------------------------
+submitButton.addEventListener('click', async function () {
+  
+  if(true){
+  
+  let usersPoolLength = usersPool.length;
+  console.log(department.value);
+  usersPool.push(new User(department.value, fio.value, storageType.value, expirationDate.value));
 
+  let response = await fetch('http://localhost:3000/person', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json;charset=utf-8'
+    },
+    body: JSON.stringify(usersPool[usersPoolLength])
+  });
+
+  let result = await response.json();
+
+ // console.log(result);
+
+  let outputBlocksTmp = document.querySelectorAll('.output-block');
+  addRow(outputBlocksTmp.length, department.value, fio.value, storageType.value, expirationDate.value, result._id);
+
+ // console.log("Writed!");
+  } else {alert('Вы ввели что-то неправильно!');}
 });
+
+
+//-----------------------PEOPLE LIST---------------------------------------
+function peopleList() {
+  fetch('http://localhost:3000/people')
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => { // we getting massive of objects from our database in variable "data"
+
+      for (let i = 0; i < data.length; i++) {
+        //         infoContainer.insertAdjacentHTML('afterbegin', `<div class="output-block">
+        // <div class="output-area">${data[i].department}</div>
+        // <div class="output-area">${data[i].fio}</div>
+        // <div class="output-area">${data[i].storageType}</div>
+        // <div class="output-area">${data[i].expirationDate}</div>
+        // <div class="output-area"><input type="button" value="Удалить" id="userinfo-delete-${i}"></div>
+        // </div>`);
+        addRow(i, data[i].department, data[i].fio, data[i].storageType, data[i].expirationDate, data[i]._id);
+      };
+    });
+};
+
+peopleList();
